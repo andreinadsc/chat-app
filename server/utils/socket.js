e4bfd55693eb9ae93f91a2156c4fca9f229cd6ca
Andreina Da Silva <andreinadsc@gmail.com>
2023-09-27 00:02:19 +0200

    chat app - first commit

--- server/utils/socket.js
+++ server/utils/socket.js
@@ -4,7 +4,7 @@ module.exports = {
     init: httpServer => {
         io = require('socket.io')(httpServer, {
             cors: {
-                origin: 'http://localhost:3000',
+                origin: 'https://chat-app-1993.netlify.app/',
                 methods: ['GET', 'POST']
             }
         });
